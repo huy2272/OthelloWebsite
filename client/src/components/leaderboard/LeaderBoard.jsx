1db02ba02adfc4de0d4750dbfc2565106203e16c
huy227 <huy2272@gmail.com>
2022-08-30 21:55:31 -0400

    top 10
    Now displaying top 10 players

--- client/src/components/leaderboard/LeaderBoard.jsx
+++ client/src/components/leaderboard/LeaderBoard.jsx
@@ -9,23 +9,50 @@ function LeaderBoard(props) {
             <BoardItem
                 title={props.items[0].title}
                 amount={props.items[0].amount}
-
             />
             <BoardItem
                 title={props.items[1].title}
                 amount={props.items[1].amount}
-
             />
             <BoardItem
                 title={props.items[2].title}
                 amount={props.items[2].amount}
-
             />
             <BoardItem
                 title={props.items[3].title}
                 amount={props.items[3].amount}
 
             />
+            <BoardItem
+                title={props.items[4].title}
+                amount={props.items[4].amount}
+            />
+            <BoardItem
+                title={props.items[5].title}
+                amount={props.items[5].amount}
+
+            />
+            <BoardItem
+                title={props.items[6].title}
+                amount={props.items[6].amount}
+
+            />
+            <BoardItem
+                title={props.items[7].title}
+                amount={props.items[7].amount}
+
+            />
+            <BoardItem
+                title={props.items[8].title}
+                amount={props.items[8].amount}
+
+            />
+            <BoardItem
+                title={props.items[9].title}
+                amount={props.items[9].amount}
+
+            />
+
         </Card>
     );
 }
